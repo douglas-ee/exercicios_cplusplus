45129e86560b58d4e9cc413668829ff9ccce5280
Douglas Gomes <douglas.gomes@ee.ufcg.edu.br>
2020-06-18 09:33:52 -0300

    ~...

---
+++ C/1_hello_world.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+//Funcao principal do programa
+void main(){
+
+    //Imprime na tela
+    printf("Oi mundo!");
+
+    //Pausa o programa apos executar
+    system("pause");
+
+}
