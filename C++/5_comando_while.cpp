8fddfd706d8867e83bd9e70620a49aab9d5d333b
Douglas Gomes <douglas.gomes@ee.ufcg.edu.br>
2020-06-19 09:50:28 -0300

    ~...

---
+++ C++/5_comando_while.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int n = 0;
+	
+	//Imprimindo somente os numeros pares de 0 a 100
+	while(n <= 100)
+	{
+		if(n % 2 != 0){
+			n++;
+			/*O continue faz com que a partir desse ponto
+			volte para o inicio O WHILE, ate que seja falso*/
+			continue;
+		}
+		cout << n << endl;
+		n++;
+	}
+	return 0;
+}
