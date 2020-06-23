84653be172a1c0c9540a95fea22c88e060d5c5e8
douglas-ee <douglas.gomes@ee.ufcg.edu.br>
2020-06-23 13:13:46 -0300

    ~...

---
+++ C/12_laco_de_repeticao_while.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+//Funcao principal do programa
+void main(){
+
+    //Definindo Vari�veis
+    int a = 1, b = 10;
+
+    //Contando ate 10
+    while(a <= 10){
+
+        //Imprimindo 'a' na tela
+        printf("\n%d", a);
+
+        //Incremento
+        a++;     //a = a + 1;
+
+    }
+
+    //Contagem Regressiva
+    while(b >= 1){
+
+        //Imprimindo 'b' na tela
+        printf("\n%d", b);
+
+        //Incremento
+        b--;    //b = b - 1;
+
+    }
+
+    //Pausa o programa apos executar
+    system("pause");
+
+}
---
+++ C/13_laco_de_repeticao_dowhile.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+//Funcao principal do programa
+void main(){
+
+    //Definindo Variaveis
+    int a = 1;
+
+    //Primeiro confere a condicao, depois repete o bloco
+    while(a <= 10){
+
+        //Imprimindo 'a' na tela
+        printf("\n%d", a);
+
+        //Incremento
+        a++;     //a = a + 1;
+
+    }
+
+    //Alterando o valor de 'a'
+    a = 20;
+
+    //Primeiro executa uma vez, depois confere a condicao
+    do{
+
+        //Imprimindo 'a' na tela
+        printf("\n%d", a);
+
+        //Incremento
+        a++;     //a = a + 1;
+
+    }while(a <= 10);
+
+    //Pausa o programa apos executar
+    system("pause");
+
+}
