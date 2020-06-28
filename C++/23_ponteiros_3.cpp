70a2dde3a96f3a0c9e679a6aa4c192d05e521ea2
douglas-ee <douglas.gomes@ee.ufcg.edu.br>
2020-06-28 08:56:43 -0300

    ~...

--- C++/23_ponteiros_3.cpp
+++ C++/23_ponteiros_3.cpp
@@ -2,59 +2,56 @@
 
 using namespace std;
 
-/* ----- Poderiamos utilizar a biblioteca <string.h>, e sua função de
-contagem de tamanho strlen(vetor) e em seguida a sua função de concatenação
-de strings, strcat(string1, string2). Abreviariamos bastante o codigo. -----*/
-
-//Função que calcula o tamanho do ponteiro
+//Funcao que calcula o tamanho do ponteiro
 int my_strlen(char *str)
 {
-	int tam = 0;
-	for(int i = 0; str[i] != '\0'; i++, tam++);
-	return tam;
+    int tam = 0;
+    for (int i = 0; str[i] != '\0'; i++, tam++)
+        ;
+    return tam;
 }
-//Função que concatena duas strings numa só
+//Funcao que concatena duas strings numa so
 char *my_strcat(char *dest, char *orig)
 {
-	char *resultado;
-	//Função que calcula o tamanho da string do destino, nome principal.
-	int tam_dest = my_strlen(dest);
-	//Função que calcula o tamanho da string da origem, sobrenome.
-	int tam_orig = my_strlen(orig);
-	int i, j = 0;
-	//Como descobrimos o tamanho do nome e do sobrenome, criamos um char "Resultado" que possuirá o tamanho da soma dos dois ja obtidos
-	resultado = new char[tam_dest + tam_orig];
-	//O ponteiro p_str está apontando para a variavel (vetor) resultado que possui certo tamanho calculado anteriormente.
-	char *p_str = resultado;
-	//Laço de reptição que define a contagem de i e j e do nome principal
-	for(i = 0; dest[i] != '\0'; i++, j++)
-	{
-		p_str[j] = dest[i];
-	}
-	//Laço de reptição que define o resto da contagem de j e do sobrenome
-	for(i = 0; orig[i] != '\0'; i++, j++)
-	{
-		p_str[j] = orig[i];
-	}
-	//Ponteiro que recebe o valor j contato, e incremente nesse endereço o vazio para fechar a impressão da string concatenada
-	p_str[j] = '\0';
+    char *resultado;
+    //Funcao que calcula o tamanho da string do destino, nome principal.
+    int tam_dest = my_strlen(dest);
+    //Funcao que calcula o tamanho da string da origem, sobrenome.
+    int tam_orig = my_strlen(orig);
+    int i, j = 0;
+    //Como descobrimos o tamanho do nome e do sobrenome, criamos um char "Resultado" que possuira o tamanho da soma dos dois ja obtidos
+    resultado = new char[tam_dest + tam_orig];
+    //O ponteiro p_str esta apontando para a variavel (vetor) resultado que possui certo tamanho calculado anteriormente.
+    char *p_str = resultado;
+    //Laco de repticao que define a contagem de i e j e do nome principal
+    for (i = 0; dest[i] != '\0'; i++, j++)
+    {
+        p_str[j] = dest[i];
+    }
+    //Laco de repticao que define o resto da contagem de j e do sobrenome
+    for (i = 0; orig[i] != '\0'; i++, j++)
+    {
+        p_str[j] = orig[i];
+    }
+    //Ponteiro que recebe o valor j contato, e incremente nesse endereco o vazio para fechar a impressao da string concatenada
+    p_str[j] = '\0';
 
-	//Retornando o resultado
-	return resultado;
+    //Retornando o resultado
+    return resultado;
 }
-//Função principal que irá receber todas as outras duas funções
+//Funcao principal que ira receber todas as outras duas funcaes
 int main()
 {
-	//Nome1 = Nome principal
-	char * nome1 = new char[100];
-	//Nome2 = Sobrenome
-	char * nome2 = new char[100];
-	cout << "Digite o primeiro nome: ";
-	cin >> nome1;
-	cout << "Digite o segundo nome: ";
-	cin >> nome2;
-	//Impressão e concatenação das duas strings
-	cout << "Resultado: " << my_strcat(nome1, nome2) << endl;
+    //Nome1 = Nome principal
+    char *nome1 = new char[100];
+    //Nome2 = Sobrenome
+    char *nome2 = new char[100];
+    cout << "Digite o primeiro nome: ";
+    cin >> nome1;
+    cout << "Digite o segundo nome: ";
+    cin >> nome2;
+    //Impressao e concatenacao das duas strings
+    cout << "Resultado: " << my_strcat(nome1, nome2) << endl;
 
-	return 0;
+    return 0;
 }
--- C++/25_sobrecarga_de_funcoes.cpp
+++ C++/25_sobrecarga_de_funcoes.cpp
@@ -2,27 +2,27 @@
 
 using namespace std;
 
-//Primeira função SOMA
+//Primeira funcao SOMA
 int soma(int n1, int n2)
 {
-	//O que diferencia uma função de outra é a quantidade argumentos
-	//Nessa são dois argumentos
-	return n1 + n2;
+    //O que diferencia uma funcao de outra eh a quantidade de argumentos
+    //Nessa sao dois argumentos
+    return n1 + n2;
 }
-//Segunda função SOMA
-int soma( int n1, int n2, int n3)
+//Segunda funcao SOMA
+int soma(int n1, int n2, int n3)
 {
-	//O que diferencia uma função de outra é a quantidade argumentos
-	//Nessa são tres argumentos
-	return n1 + n2 + n3;
+    //O que diferencia uma funcao de outra eh a quantidade de argumentos
+    //Nessa sao tres argumentos
+    return n1 + n2 + n3;
 }
-//Função principal 
+//Funcao principal
 int main()
 {
-	//Chamdno a primeira função SOMA
-	cout << soma(1, 2) << endl;
-	//Chamdno a segunda função SOMA
-	cout << soma(1, 2, 3) << endl;
+    //Chamdno a primeira funcao SOMA
+    cout << soma(1, 2) << endl;
+    //Chamdno a segunda funcao SOMA
+    cout << soma(1, 2, 3) << endl;
 
-	return 0;
+    return 0;
 }
