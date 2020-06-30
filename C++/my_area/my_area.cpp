50bf33ec8096195b5c27734cfd02fecca4e0428f
douglas-ee <douglas.gomes@ee.ufcg.edu.br>
2020-06-30 20:45:01 -0300

    ~...

--- C++/my_area/my_area.cpp
+++ C++/my_area/my_area.cpp
@@ -2,25 +2,25 @@
 
 float a_quadrado(float lado)
 {
-	return lado * lado;
+    return lado * lado;
 }
 float a_retangulo(float altura, float comprimento)
 {
-	return altura * comprimento;
+    return altura * comprimento;
 }
 float a_triangulo(float lado)
 {
-	return (lado * lado) / 2;
+    return (lado * lado) / 2;
 }
 float a_trapezio(float lado_a, float lado_b, float altura)
 {
-	return ((lado_a + lado_b) * altura) / 2;
+    return ((lado_a + lado_b) * altura) / 2;
 }
 float a_circulo(float raio)
 {
-	return 3.1415 * raio * raio;
+    return 3.1415 * raio * raio;
 }
 float a_esfera(float raio)
 {
-	return 4 * 3.1415 * raio * raio;
+    return 4 * 3.1415 * raio * raio;
 }
