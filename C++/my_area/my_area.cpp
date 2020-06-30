d126874e894666d8aa119270bdae4999265b9ca0
douglas-ee <douglas.gomes@ee.ufcg.edu.br>
2020-06-30 20:40:53 -0300

    ~...

---
+++ C++/my_area/my_area.cpp
@@ -0,0 +1,26 @@
+// ESSE MÓDULO CONTEM FUNÇÕES MATEMATICAS DE CALCULO DE AREA
+
+float a_quadrado(float lado)
+{
+	return lado * lado;
+}
+float a_retangulo(float altura, float comprimento)
+{
+	return altura * comprimento;
+}
+float a_triangulo(float lado)
+{
+	return (lado * lado) / 2;
+}
+float a_trapezio(float lado_a, float lado_b, float altura)
+{
+	return ((lado_a + lado_b) * altura) / 2;
+}
+float a_circulo(float raio)
+{
+	return 3.1415 * raio * raio;
+}
+float a_esfera(float raio)
+{
+	return 4 * 3.1415 * raio * raio;
+}
---
+++ C++/my_area/my_area.h
@@ -0,0 +1,11 @@
+#ifndef _MY_AREA_H_
+#define _MY_AREA_H_
+
+float a_quadrado(float lado);
+float a_retangulo(float altura, float comprimento);
+float a_triangulo(float lado);
+float a_trapezio(float lado_a, float lado_b, float altura);
+float a_circulo(float raio);
+float a_esfera(float raio);
+
+#endif
